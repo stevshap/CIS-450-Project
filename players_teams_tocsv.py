69a383e6eea252ce1ea7093cf45d741ad93a15d8
c0rmsby <44877463+c0rmsby@users.noreply.github.com>
2020-11-18 20:15:20 -0500

    adapted python scripts to generate our csv data

---
+++ players_teams_tocsv.py
@@ -0,0 +1,47 @@
+#### code adapted/modified from https://github.com/dolthub/dolthub-etl-jobs/blob/master/adhoc/nfl-play-by-play/import-rosters.py #####
+
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/'
+roster_path = 'roster-data/roster.csv.gz'
+
+
+# read rosters csv from which we get teams.csv and players.csv
+rosters_url = url + roster_path
+rosters_df = pd.read_csv(rosters_url,
+                          compression='gzip',
+                          low_memory=False)
+
+# get teams table data from rosters_df into a dataframe for cleaning, then to csv for database population
+filter_col = [col for col in rosters_df if col.startswith('team.')]
+
+teams_df = rosters_df[filter_col]
+teams_df = teams_df.drop_duplicates()
+teams_df = teams_df.rename(columns=lambda x: x.replace('team.', ''))
+
+# drop columns we excluded in our schema
+teams_df = teams_df.drop(['abbr', 'fullName', 'conferenceAbbr', 'divisionAbbr'], axis=1)
+
+# export to csv
+teams_df.to_csv('teams.csv', index=False)
+
+# get players table data from rosters_df into a dataframe for cleaning, then to csv for database population
+filter_col = [col for col in rosters_df if col.startswith('teamPlayers.')]
+filter_col = filter_col + ['team.season', 'team.teamId']
+
+players_df = rosters_df[filter_col]
+players_df = players_df.rename(columns=lambda x: x.replace('team.', ''))
+players_df = players_df.rename(columns=lambda x: x.replace('teamPlayers.', ''))
+
+# prepare columns to keep and columns to exclude during clean, based on our original schema
+# college may not be in our schema but should be included
+cols_tokeep = ['nflId', 'season', 'teamId', 'displayName', 'status', 'position', 'gsisId', 'hometown', 'collegeID', 'college']
+exclude = []
+for col in players_df.columns.tolist():
+    if col not in cols_tokeep:
+        exclude.append(col)
+
+players_df = players_df.drop(exclude, axis=1)
+
+# export to csv
+players_df.to_csv('players.csv', index=False)
---
+++ plays_tocsv.py
@@ -0,0 +1,30 @@
+#### code adapted/modified from https://github.com/dolthub/dolthub-etl-jobs/blob/master/adhoc/nfl-play-by-play/import-play-by-play.py #####
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/'
+
+plays_df = pd.DataFrame()
+
+# start grabbing plays data from 2000 up to and including 2019
+year = 2000
+while year < 2020:
+    url_i = url + 'play_by_play_' + str(year) + '.csv.gz?raw=True'
+    i_data = pd.read_csv(url_i, compression='gzip', low_memory=False)
+    plays_df = plays_df.append(i_data, sort=True)
+
+    year += 1
+
+# Give each row a unique index
+plays_df.reset_index(drop=True, inplace=True)
+
+# Clean columns
+to_keep = ['playId', 'gameId', 'home_team', 'away_team', 'desc', 'play_type', 'rusher_player_id', 'passer_player_id', 'receiver_player_id']
+exclude = []
+
+for col in plays_df.columns.tolist():
+    if col not in to_keep:
+        exclude.append(col)
+
+plays_df.drop(exclude, axis=1)
+
+plays_df.to_csv('plays.csv', index=False)
